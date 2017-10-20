a8565a1141918790e0c90734c5d2cff2fbe6f951
Alex Filatau <filatau@gmail.com>
2017-10-20 16:34:36 -0700

    Reformat some aggregation notes

--- mongo_aggregation.js
+++ mongo_aggregation.js
@@ -1,43 +1,51 @@
-> db.posts.aggregate(
+db.posts.aggregate(
   [
-    {$unwind: "$comments"},
-    {$group:{_id:"$comments.author", "count": {$sum: 1}}},
-    {$sort: {"count" : -1}},
+    {$unwind: '$comments'},
+    {$group: {_id: '$comments.author', 'count': {$sum: 1}}},
+    {$sort: {'count': -1}},
     {$limit: 5}
   ]
 )
 
-
 db.messages.aggregate(
-	[
-	{$unwind: "$headers.To"},
-	{$group:{
-			_id:{"from": "$headers.From", "to": "$headers.To"},
-			count: {$sum: 1}
-		}
-	},
-    {$sort: {"count" : -1}},
+  [
+  {$unwind: '$headers.To'},
+  // remove duplicates and rebuild
+    {$group:
+    {
+      _id: {'_id': '$_id',
+      From: '$headers.From'},
+      To: {$addToSet: '$headers.To'}
+    }
+  },
+  {$unwind: '$To'},
+  {$group: {
+      _id: {'From': '$_id.From', 'to': '$To'},
+      count: {$sum: 1}
+    }
+  },
+    {$sort: {'count': -1}},
     {$limit: 5}
-	]
+  ]
 )
 
 db.zips.aggregate(
   [
-    {$match:{
-      $or:[
-        {state:"CA"},
-        {state: "NY"}
+    {$match: {
+      $or: [
+        {state: 'CA'},
+        {state: 'NY'}
       ]
     }},
     {
       $match: {
-        "pop": {$gt: 25000}
+        'pop': {$gt: 25000}
       }
     },
     {
       $group: {
-        "_id": null,
-        avg: {"$avg": "$pop"}
+        '_id': null,
+        avg: {'$avg': '$pop'}
       }
     }
   ]
@@ -45,17 +53,17 @@ db.zips.aggregate(
 
 db.grades.aggregate(
   [
-    {"$unwind": "$scores"},
-    {"$match": {
-      $or: [ {"scores.type": "homework"},{"scores.type": "exam" } ]
+    {'$unwind': '$scores'},
+    {'$match': {
+      $or: [ {'scores.type': 'homework'}, {'scores.type': 'exam' } ]
     }},
     {$group: {
-      "_id": {"class": "$class_id", "student": "$student_id"},
-      "student_average": {"$avg": "$scores.score"}
+      '_id': {'class': '$class_id', 'student': '$student_id'},
+      'student_average': {'$avg': '$scores.score'}
     }},
     {
       $group: {
-        "_id": "$_id.class", "class_average": {"$avg": "$student_average"}
+        '_id': '$_id.class', 'class_average': {'$avg': '$student_average'}
       }
     },
     {$sort: {class_average: -1}}
@@ -65,17 +73,17 @@ db.grades.aggregate(
 db.zips.aggregate([
     {$project:
       {
-        first_char: {$substr : ["$city",0,1]},
-        population: "$pop"
+        first_char: {$substr : ['$city',0,1]},
+        population: '$pop'
       }
     },
     {
       $match:
       {
-        "first_char": {$in: ["B", "D", "O", "G", "N","M"]}
+        'first_char': {$in: ['B', 'D', 'O', 'G', 'N','M']}
       }
     },
     {
-      $group: {"_id": null, total: {$sum:"$population"}}
+      $group: {'_id': null, total: {$sum:'$population'}}
     }
 ])
